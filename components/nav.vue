3e771e878b5d779ac02c3cde82f14f1595b8745b
atav1k <atavikram@gmail.com>
2018-08-16 18:19:35 -0700

    Initial commit

---
+++ components/nav.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+  content
+    nav.nav
+      Vikram Babu
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  main
+    section.cols
+      .col
+        h1 atav1k
+        h2 Portfolio
+</template>
