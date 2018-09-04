672f51f3790a3dee7566f8153e8e3fa3cdbf7f99
atav1k <atavikram@gmail.com>
2018-09-04 13:58:25 -0700

    flow edits

--- pages/index.vue
+++ pages/index.vue
@@ -17,14 +17,14 @@
       .col.marT2
         h3#top
           | Hi, I'm Vikram. I implement 
-          span.serif Design Systems
+          span.serif design systems
           | , coach teams on 
-          span.serif Design Operations,
+          span.serif design operations,
           |  and de-risk projects with 
-          span.serif Product Discovery
+          span.serif product discovery
           | .
         h4 I recently worked at 
-          a(href="https://gigster.com") Gigster
+          span.serif Gigster
           |  as a Senior Product Designer where I led application design and usability using design systems for our internal tools and marketing products.
         h4 I'm currently available for work. Take a look at my 
           a(href="/img/vikram-resume.pdf" role="link") resume
@@ -32,7 +32,7 @@
         fold
           h4 Over my design career I've been a product design fellow and held roles in visual design and frontend at agencies and in-house. I also launched the publication 
             a(href="https://medium.com/endless") Endless
-            | growing it to over 30k+ subscribers and 5M+ reads in its brief year of existence. I was also a regular contributor to 
+            |  growing it to over 30k+ subscribers and 5M+ reads in its brief year of existence. I was also a regular contributor to 
             span.serif Medium's
             |  technology focused 
             a(href="https://medium.com/message") Message
